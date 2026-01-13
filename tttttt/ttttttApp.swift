320462597ad5a684799746dcd5fb6d51218d4b20
jeongmu <jeongmuya@gmail.com>
2026-01-14 04:53:24 +0900

    Initial Commit

---
+++ tttttt/ttttttApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttttttApp.swift
+//  tttttt
+//
+//  Created by 정무 on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ttttttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
